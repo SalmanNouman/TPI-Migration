6ff34e54e2a3ff708fac437ca9ea64fd2e75f2f3
David Inglis <dinglis@conestogac.on.ca>
2023-08-03 11:23:01 -0400

    CORE-248 - Initial template for DLX project repository
    * Using Unity 2022.3.6f1 which is the latest LTS release as of 2023-08-03
    * Adding Unity packages Cinemachine and TextMeshPro
    * Adding CORE UPM Registry with SCORM package

---
+++ Assets/Scripts/Sample.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VARLab.Example
+{
+    public class Sample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Hi");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# DLX Project Template
+
+This repository contains a template for a DLX project using Unity's **Universal Render Pipeline** and **CORE Framework** packages.
