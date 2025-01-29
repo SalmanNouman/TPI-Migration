f73b1be707077b0cb15920035bf28fea3321b719
Ktheoret <Kristin.Theoret@gmail.com>
2025-01-29 10:30:57 -0500

    TPI-23 Created an abstract task class with a HandleTask Method and three events (OnTaskStarted, OnTaskCompleted, OnTaskFailed).

---
+++ Assets/Scripts/Tasks/Task.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace VARLab.DLX
+{
+    public abstract class Tasks
+    {
+        //events
+        public UnityEvent OnTaskStarted;
+        public UnityEvent OnTaskCompleted;
+        public UnityEvent OnTaskFailed;
+
+        //methods
+        public abstract void HandleTask();
+    }
+}
