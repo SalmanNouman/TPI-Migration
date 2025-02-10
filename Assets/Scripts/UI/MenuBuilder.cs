6548549059d51b3f5085bc0170e20f5f453f114b
Carolina Naoum Junqueira <cnaoumjunqueira@conestogac.on.ca>
2025-02-10 19:00:02 +0000

    Merged in TPI-55-add-menu-buttons (pull request #13)
    TPI-55 add menu buttons (Pause, Inspection Review and Settings)
    
    * TPI-55 adding menu buttons to the scene
    
    * created the menu buttons UI Document
    * add three instances of the Button-Icon-Round-Large template
    * created the menu style sheet
    * added the sprite variables to the TPI light and Dark Themes
    * created a menu builder script and added events to each one of the buttons
    
    * TPI-55 - added tests for menu buttons
    
    * creates a menu buttons test scene
    
    * TPI-55 added comments to test and updated the gitignore to include code coverage files
    
    * TPI-55 added the class header to menu builder class
    
    * Merge branch 'main' into TPI-55-add-menu-buttons
    
    # Conflicts:
    #	ProjectSettings/EditorBuildSettings.asset
    
    * Resolved conflict using main
    
    * TPI-55 added the button test scene to the build settings
    
    
    Approved-by: Juhwan Seo

---
+++ Assets/Scripts/UI/MenuBuilder.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+using VARLab.Velcro;
+
+namespace VARLab.DLX
+{
+    /// <summary>
+    /// This class is responsible for setting up, building and managing the menu buttons and its events.
+    /// </summary>
+    public class MenuBuilder : MonoBehaviour, IUserInterface
+    {
+        /// <summary>
+        /// Reference to the UI document root visual element
+        /// </summary>
+        private VisualElement root;
+
+        /// <summary>
+        /// Menu buttons
+        /// </summary>
+        private Button pauseButton;
+        private Button inspectionReviewButton;
+        private Button settingsButton;
+
+        /// <summary>
+        /// Button Events
+        /// </summary>
+        public UnityEvent OnPauseClicked;
+        public UnityEvent OnInspectionReviewClicked;
+        public UnityEvent OnSettingsClicked;
+
+        /// <summary>
+        /// Gets the references to all the buttons and the root visual element.
+        /// </summary>
+        private void Start()
+        {
+            root = gameObject.GetComponent<UIDocument>().rootVisualElement;
+            pauseButton = root.Q<Button>("Pause");
+            inspectionReviewButton = root.Q<Button>("InspectionReview");
+            settingsButton = root.Q<Button>("Settings");
+
+            SetListeners();
+        }
+
+        private void Awake()
+        {
+            OnPauseClicked ??= new();
+            OnInspectionReviewClicked ??= new();
+            OnSettingsClicked ??= new();
+        }
+
+        /// <summary>
+        /// Sets the buttons clicked events for all the buttons
+        /// </summary>
+        private void SetListeners()
+        {
+            pauseButton.clicked += PauseClicked;
+            inspectionReviewButton.clicked += InspectionReviewClicked;
+            settingsButton.clicked += SettingsClicked;
+        }
+
+        /// <summary>
+        /// Actions linked to the pause button clicked event.
+        /// Invokes the OnPauseclicked event.
+        /// </summary>
+        private void PauseClicked()
+        {
+            OnPauseClicked?.Invoke();
+            Debug.Log("Pause button clicked");
+        }
+
+        /// <summary>
+        /// Actions linked to the inspection review button clicked event.
+        /// Invokes the OnInspectionReviewClicked event.
+        /// </summary>
+        private void InspectionReviewClicked()
+        {
+            OnInspectionReviewClicked?.Invoke();
+            Debug.Log("Inspection review button clicked");
+        }
+
+        /// <summary>
+        /// Actions linked to the settings button clicked event.
+        /// Invokes the OnSettingsClicked event.
+        /// </summary>
+        private void SettingsClicked()
+        {
+            OnSettingsClicked?.Invoke();
+            Debug.Log("Settings button clicked");
+        }
+
+        /// <summary>
+        /// Enables and disables the menu buttons
+        /// </summary>
+        /// <param name="toggle">true = buttons enabled, false = buttons disabled</param>
+        public void ToggleButtons(bool toggle)
+        {
+            pauseButton.SetEnabled(toggle);
+            inspectionReviewButton.SetEnabled(toggle);
+            settingsButton.SetEnabled(toggle);
+        }
+
+        /// <summary>
+        /// Hides the menu buttons
+        /// </summary>
+        public void Hide()
+        {
+            UIHelper.Hide(root);
+        }
+
+        /// <summary>
+        /// Displays the menu buttons
+        /// </summary>
+        public void Show()
+        {
+            UIHelper.Show(root);
+        }
+    }
+}
---
+++ Assets/Tests/Play Mode/MenuIntegrationTests.cs
@@ -0,0 +1,199 @@
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UIElements;
+using VARLab.DLX;
+
+namespace Tests
+{
+    public class MenuIntegrationTests
+    {
+        private GameObject menuObject;
+        private UIDocument menuDocument;
+        private MenuBuilder menuBuilder;
+
+        private int sceneCounter = 0;
+        private const string SceneName = "MenuButtonsTestScene";
+
+        /// <summary>
+        /// Loads the menu buttons test scene
+        /// </summary>
+        [OneTimeSetUp]
+        [Category("BuildServer")]
+        public void RunOnce()
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+
+
+        /// <summary>
+        /// Checks if the test scene is loaded
+        /// </summary>
+        [UnityTest, Order(0)]
+        [Category("BuildServer")]
+        public IEnumerator SceneLoaded()
+        {
+            yield return new WaitUntil(() => SceneManager.GetSceneByName(SceneName).isLoaded);
+
+            menuBuilder = GameObject.FindAnyObjectByType<MenuBuilder>();
+            menuDocument = menuBuilder.GetComponent<UIDocument>();
+
+            Assert.IsTrue(SceneManager.GetSceneByName(SceneName).isLoaded);
+        }
+
+        /// <summary>
+        /// Checks if the UI buttons are visible when Show is called
+        /// </summary>
+        [UnityTest, Order(1)]
+        [Category("BuildServer")]
+        public IEnumerator CheckIfUIIsEnabledOnShow()
+        {
+            // Arrange
+            var expectedResults = DisplayStyle.Flex.ToString().Trim();
+            VisualElement root = menuDocument.rootVisualElement;
+
+            // Act
+            menuBuilder.Hide();
+            yield return new WaitForSeconds(1f);
+            menuBuilder.Show();
+            yield return new WaitForSeconds(1f);
+
+            // Assert
+            Assert.AreEqual(expectedResults, root.style.display.ToString().Trim());
+        }
+
+        /// <summary>
+        /// Checks if the UI is hidden on Hide
+        /// </summary>
+        [UnityTest, Order(2)]
+        [Category("BuildServer")]
+        public IEnumerator CheckIfUIIsDisabledOnHide()
+        {
+            // Arrange
+            var expectedResults = DisplayStyle.None.ToString().Trim();
+            VisualElement root = menuDocument.rootVisualElement;
+
+            // Act
+            menuBuilder.Hide();
+            yield return new WaitForSeconds(1f);
+
+            // Assert
+            Assert.AreEqual(expectedResults, root.style.display.ToString().Trim());
+        }
+
+        /// <summary>
+        /// Checks if the OnPauseClicked event is triggered when the pause button is clicked
+        /// </summary>
+        [UnityTest, Order(3)]
+        [Category("BuildServer")]
+        public IEnumerator PauseButtonClickTriggersOnPauseClickedEvent()
+        {
+            // Arrange
+            bool wasClicked = false;
+            bool expectedResult = true;
+            VisualElement root = menuDocument.rootVisualElement;
+            Button button = root.Q<Button>("Pause");
+            menuBuilder.Show();
+            menuBuilder.OnPauseClicked.AddListener(() => wasClicked = true);
+
+            // Act
+            var e = new NavigationSubmitEvent() { target = button };
+            button.SendEvent(e);
+
+            yield return null;
+
+            // Assert
+            Assert.AreEqual(expectedResult, wasClicked);
+        }
+
+        /// <summary>
+        /// Checks if the OnInspectionReviewClicked event is triggered when the inspection review button is clicked
+        /// </summary>
+        [UnityTest, Order(4)]
+        [Category("BuildServer")]
+        public IEnumerator InspectionReviewClickTriggersOnInspectionReviewClickedEvent()
+        {
+            // Arrange
+            bool wasClicked = false;
+            bool expectedResult = true;
+            VisualElement root = menuDocument.rootVisualElement;
+            Button button = root.Q<Button>("InspectionReview");
+
+            menuBuilder.OnInspectionReviewClicked.AddListener(() => wasClicked = true);
+
+            // Act
+            var e = new NavigationSubmitEvent() { target = button };
+            button.SendEvent(e);
+
+            yield return null;
+
+            // Assert
+            Assert.AreEqual(expectedResult, wasClicked);
+        }
+
+        /// <summary>
+        /// Checks if the OnSettingsClicked event is triggered when the settings button is clicked
+        /// </summary>
+        [UnityTest, Order(5)]
+        [Category("BuildServer")]
+        public IEnumerator SettingsClickTriggersOnSettingsClickedEvent()
+        {
+            // Arrange
+            bool wasClicked = false;
+            bool expectedResult = true;
+            VisualElement root = menuDocument.rootVisualElement;
+            Button button = root.Q<Button>("Settings");
+
+            menuBuilder.OnSettingsClicked.AddListener(() => wasClicked = true);
+
+            // Act
+            var e = new NavigationSubmitEvent() { target = button };
+            button.SendEvent(e);
+
+            yield return null;
+
+            // Assert
+            Assert.AreEqual(expectedResult, wasClicked);
+        }
+
+        /// <summary>
+        /// Checks if none of the button events get triggered when the buttons as disabled
+        /// </summary>
+        [UnityTest, Order(6)]
+        [Category("BuildServer")]
+        public IEnumerator ButtonClickedIsNotTriggeredWhenButtonsAreDisabled()
+        {
+            // Arrange
+            bool wasClicked = false;
+            bool expectedResult = false;
+            bool enabled = true;
+            VisualElement root = menuDocument.rootVisualElement;
+            Button pauseButton = root.Q<Button>("Pause");
+            Button inspectionReviewButton = root.Q<Button>("InspectionReview");
+            Button settingsButton = root.Q<Button>("Settings");
+
+            menuBuilder.OnPauseClicked.AddListener(() => wasClicked = true);
+            menuBuilder.OnInspectionReviewClicked.AddListener(() => wasClicked = true);
+            menuBuilder.OnSettingsClicked.AddListener(() => wasClicked = true);
+
+            // Act
+            menuBuilder.ToggleButtons(!enabled);
+
+            var pauseEvt = new NavigationSubmitEvent() { target = pauseButton };
+            pauseButton.SendEvent(pauseEvt);
+
+            var inspectionReviewEvt = new NavigationSubmitEvent() { target = inspectionReviewButton };
+            inspectionReviewButton.SendEvent(inspectionReviewEvt);
+
+            var settingsEvt = new NavigationSubmitEvent() { target = settingsButton };
+            settingsButton.SendEvent(settingsEvt);
+
+            yield return null;
+
+            // Assert
+            Assert.AreEqual(expectedResult, wasClicked);
+        }
+    }
+}
