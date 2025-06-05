62ec06d3e63b5ea51fe126321d306292b60f7d16
Karandeep Sandhu <ksandhu@conestogac.on.ca>
2025-06-05 19:41:45 +0000

    Merged in TPI-318-camera-repositioned-certain-navs (pull request #150)
    TPI-318 camera repositioned certain navs
    
    * TPI-318 Add class that allows setting camera target on specific waypoints
    
    * TPI-318 Add event listener for walk completed instead of having to serialize event
    
    * Did so to prevent cases where if an event is not hooked up properly, it would lock the game as the camera lock target would still persist thus preventing panning.
    
    * TPI-318 Refactors
    
    * Rename method names
    * Remove redundant event by using one event for setting camera look at target
    
    * TPI-318 Add script to point and click navigation game object
    
    * TPI-318 Add camera targets for piercing area
    
    * Add prototype of feature to have more than one camera target per waypoint for after NPC
    
    * TPI-318 Add tooltips for clarity
    
    * TPI-318 Refactor to make it serialization more organized in editor by allowing grouping with name
    
    * TPI-318 Update prefab with camera targets and main scene with serialization of camera targets for waypoint
    
    * Merge branch 'main' into TPI-318-camera-repositioned-certain-navs
    Fix conflcts with main scene
    
    * TPI-318 Fix missing event in point and click nav
    
    * TPI-318 Fix reception waypoint looking at floor
    
    - One time target was added that was the waypoint itself, now removed.
    
    * TPI-318 Add two waypoint camera targets and remove one
    
    * Removed camera target for piercing room for middle of room on entrance waypoint
    * Add waypoint camera target for corner chair to prevent staring at wall when coming from washing station in tattoo room
    * Add waypoint camera target for reprocessing area initial entrace waypoint so to not make player look at white wall on entrance. (This one I made one time trigger since the door can be in view and not just blank wall to staret at)
    
    * TPI-318 Add suggested refactor code
    
    * The structure improved and redundancy was removed
    
    * TPI-318 Add comments
    
    * Merge branch 'main' into TPI-318-camera-repositioned-certain-navs
    
    # Conflicts:
    #	Assets/Scenes/Main Scene.unity
    
    * TPI-318 Fix camera not resetting look at target & one time target
    
    * Merge branch 'main' into TPI-318-camera-repositioned-certain-navs
    
    # Conflicts:
    #	Assets/Scenes/Main Scene.unity
    
    
    Approved-by: Salman Nouman Abulqasim
    Approved-by: Carolina Naoum Junqueira

---
+++ Assets/Scripts/Navigation/NavigationCamHandler.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using VARLab.Navigation.PointClick;
+
+namespace VARLab.DLX
+{
+    public class NavigationCamHandler : MonoBehaviour
+    {
+        [SerializeField] private List<TargetsInPoi> targetsInPoi = new();
+        private PointClickNavigation nav;
+
+        private void Start()
+        {
+            nav = GetComponent<PointClickNavigation>();
+            nav.WalkCompleted.AddListener((_) => ResetLookAt());
+        }
+        
+        /// <summary>
+        /// Checks if the waypoint has camera target to look at triggers it
+        /// </summary>
+        /// <param name="waypoint">The waypoint to check for target for</param>
+        /// <see cref="PointClickNavigation.WalkStarted"/>
+        public void TriggerLookAt(Waypoint waypoint)
+        {
+            var target = targetsInPoi
+                .SelectMany(t => t.TargetsWithLookAt) // Flatten all targets into a single list
+                .FirstOrDefault(t => t.Waypoint == waypoint); // Search for the specific waypoint
+            if (target != null)
+            {
+                if ( target.OneTimeTarget != null && !target.OneTimeTargetTriggered)
+                {
+                    nav.LookAtTarget = target.OneTimeTarget.transform;
+                    target.OneTimeTargetTriggered = true;
+                }
+                else
+                {
+                    nav.LookAtTarget = target.Target;
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Clears the camera look at target to allow for panning and removes listener for walk complete
+        /// </summary>
+        /// <see cref="PointClickNavigation.WalkCompleted"/>s
+        public void ResetLookAt()
+        {
+            nav.LookAtTarget = null;
+        }
+    }
+
+    
+    /// <summary>
+    /// Data class to hold animation details for drilling
+    /// </summary>
+    /// <remarks>
+    /// Waypoint : the waypoint that will trigger the camera look at
+    /// Camera Target: What camera will look at
+    /// </remarks>
+    [Serializable]
+    public class TargetsWithLookAt
+    {
+        [Tooltip("Waypoint that will trigger camera look at")] public Waypoint Waypoint;
+        [Tooltip("Target for camera to look at on waypoint")] public Transform Target;
+        [Tooltip("Target for camera that is used one time initially then defaults to Target")] public Transform OneTimeTarget;
+        [HideInInspector] public bool OneTimeTargetTriggered;
+    }
+
+    /// <summary>
+    /// Container for easier organization for unity editor serialization side
+    /// </summary>
+    [Serializable]
+    public class TargetsInPoi
+    {
+        [Tooltip("Inspectable Object POI")]
+        public PoiList.PoiName Location;
+        public List<TargetsWithLookAt> TargetsWithLookAt;
+    }
+    
+}
